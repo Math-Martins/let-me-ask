442038730c05a4f467ed0fa05ba17fc0f8c50fc2
Math-Martins <mhantunes.martins@gmail.com>
2025-07-10 15:03:36 -0300

    first commit: questions count

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -15,7 +15,7 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
       .from(schema.rooms)
       .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
       .groupBy(schema.rooms.id)
-      .orderBy(schema.rooms.createAt); // schema.rooms é para referenciar uma tabela, colocando o nome do campo em seguida
+      .orderBy(schema.rooms.createAt); // schema.rooms é para referenciar uma tabela, colocando o nome do campo em seguida.
 
     return results;
   });
